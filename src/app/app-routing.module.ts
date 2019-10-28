fae858a61cd0a0ea99a2aee01bd16c962947678a
Marcus Krahl <Marcus.Krahl@t-systems.com>
2019-10-28 18:05:19 +0100

    add repro for angular bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { Test2Component } from './test2/test2.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'test', component: TestComponent},
+  { path: 'test2', component: Test2Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
